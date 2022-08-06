dc8f74453c29067efe1c3ce0f505409cd6ddb924
unknown <vradriano@hotmail.com>
2022-08-06 15:00:47 -0300

    FlatList, statusbar, alert

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Text, TextInput, View, TouchableOpacity, ScrollView } from 'react-native'
+import { Text, TextInput, View, TouchableOpacity, FlatList, Alert } from 'react-native'
 import { styles } from './styles'
 
 import { Participant } from '../components/Participant'
@@ -7,11 +7,22 @@ export default function Home() {
   const participants = ['Vitor', 'Joana', 'Rodrigo', 'Diego', 'Letícia', 'Eduardo', 'João', 'Maria', 'Álvaro', 'Valdir'];
 
   function handleParticipantAdd() {
-    console.log('oi')
+    if(participants.includes('Vitor')) {
+      Alert.alert('Participante Existe', "Já existe um participante com esse nome na nossa lista!")
+    }
   }
 
   function handleParticipantRemove( name: string ) {
-    console.log( `You have clicked on removed button and removed ${name}` )
+    Alert.alert('Remover', `Você realmente deseja remover o usuário ${name} ?`, [
+      {
+        text: 'Sim',
+        onPress: () => Alert.alert('Deletado!')
+      },
+      {
+        text: 'Não',
+        style: 'cancel'
+      }
+    ])
   }
 
   return (
@@ -40,18 +51,20 @@ export default function Home() {
           </Text>
         </TouchableOpacity>
       </View>
+
+      <FlatList 
+        keyExtractor={item => item}
+        data={participants}
+        renderItem={({ item }) => (
+          <Participant
+            key={item}
+            name={item} 
+            onRemove={() => handleParticipantRemove(item)} 
+          />
+        )}
+        ListEmptyComponent={() => (<Text style={styles.listEmptyText}>Sua lista está vazia!</Text>)}
+      />
       
-      <ScrollView showsVerticalScrollIndicator={false} >
-        {
-          participants.map(participant => (
-            <Participant
-            key={participant}
-            name={participant} 
-            onRemove={() => handleParticipantRemove(participant)} 
-            />
-          ))
-        }
-      </ScrollView>
     </View>
   )
 }
